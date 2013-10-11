b4bc858b654f232c631b07d144c14d3eb51c662c
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-11 14:47:28 +0300

    gtest main

--- test/test_safenet.cpp
+++ test/test_safenet.cpp
@@ -1,7 +1,11 @@
 //---------------------------------------------------------
-//
+// Mehmet Suyuti Dindar
+// 11.10.2013
+// SmartSoft
 //---------------------------------------------------------
 #include "gtest/gtest.h"
+
+#include "safenetHelper_tests.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
