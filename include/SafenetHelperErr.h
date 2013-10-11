11d179db8364768beda0764efc4137c215bb0456
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-11 14:48:59 +0300

    kütüphane ortak tipleri

---
+++ include/SafenetHelperErr.h
@@ -0,0 +1,12 @@
+//---------------------------------------------------------
+// Mehmet Suyuti Dindar
+// 11.10.2013
+// SmartSoft
+//---------------------------------------------------------
+
+#ifndef _SAFENET_HELPER_ERR_H_
+#define _SAFENET_HELPER_ERR_H_
+
+#define SUCCESS 0
+
+#endif// _SAFENET_HELPER_ERR_H_
---
+++ include/SafenetHelperTypes.h
@@ -0,0 +1,16 @@
+//---------------------------------------------------------
+// Mehmet Suyuti Dindar
+// 11.10.2013
+// SmartSoft
+//---------------------------------------------------------
+
+#ifndef _SAFENET_HELPER_TYPES_H_
+#define _SAFENET_HELPER_TYPES_H_
+
+#include <vector>
+
+using namespace std;
+
+typedef std::vector<unsigned char*> VectorUChar;
+
+#endif //_SAFENET_HELPER_TYPES_H_
