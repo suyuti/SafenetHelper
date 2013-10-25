4a267376d9de15a1b6e652d94026b33da38a4b5b
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-25 16:25:39 +0300

    testlerde düzenleme

--- test/cryptoHelper_Tests/cryptokiHelper_tests.h
+++ test/cryptoHelper_Tests/cryptokiHelper_tests.h
@@ -5,57 +5,28 @@
 #include "../../src/cryptokiHelper/CryptokiHelper.h"
 #include "../../src/cryptokiHelper/DataObject.h"
 #include "../../src/cryptokiHelper/ExceptionCryptoki.h"
+#include "cryptokiHelperTestUtil.h"
 
 class CryptokiHelperTests : public ::testing::Test
 {
-public:
-	class CryptokiHelperEx : public Cryptoki::CryptokiHelper {
-	public:
-		static void ClearSlot(unsigned long slotId, std::string& pin) {
-			Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
-			pC->close();
-			pC->open(slotId, pin);
-
-			int rv;
-		    CK_ULONG numObjectsToFind = 500;
-		    CK_ULONG numObjectsFound = 0;
-
-		    rv = C_FindObjectsInit(pC->getSessionHandle(), NULL, 0);
-		    if (rv != CKR_OK)
-		    	return;
-
-		    CK_OBJECT_HANDLE h[500];
-		    rv = C_FindObjects(pC->getSessionHandle(), h, numObjectsToFind,  &numObjectsFound);
-		    if (rv != CKR_OK) {
-		    	return;
-		    }
-
-		    rv = C_FindObjectsFinal(pC->getSessionHandle());
-		    if (rv != CKR_OK)
-		    	return;
-
-		    if (numObjectsFound == 0) {
-				pC->close();
-		    	return;
-		    }
-
-			for (CK_ULONG i = 0; i < numObjectsFound; ++i) {
-				C_DestroyObject(pC->getSessionHandle(), h[i]);
-			}
-			pC->close();
-		};
-	};
 public:
     virtual void SetUp() {
     }
 
     virtual void TearDown() {
     	std::string pin("1234");
-		CryptokiHelperEx::ClearSlot(1L, pin);
+    	CryptokiHelperTestUtil::ClearSlot(1L, pin);
     }
 };
 
-TEST_F(CryptokiHelperTests, negative_open_slot) {
+//-----------------------------------------------------------------------------
+/**
+ * Tries to open non existent slot.
+ * Should throw Exception.
+ *
+ * */
+
+TEST_F(CryptokiHelperTests, negative_open_with_wrong_slot) {
 	EXPECT_THROW(
 		{
 			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
@@ -67,7 +38,14 @@ TEST_F(CryptokiHelperTests, negative_open_slot) {
 		ExceptionCryptoki);
 }
 
-TEST_F(CryptokiHelperTests, negative_open_pin) {
+//-----------------------------------------------------------------------------
+/**
+ * Tries to open slot with wrong PIN
+ * Should throw exception
+ *
+ * */
+
+TEST_F(CryptokiHelperTests, negative_open_with_wrong_pin) {
 	EXPECT_THROW(
 		{
 			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
@@ -78,287 +56,41 @@ TEST_F(CryptokiHelperTests, negative_open_pin) {
 		},
 		ExceptionCryptoki);
 }
-//
-//// Key
-//
-TEST_F(CryptokiHelperTests, create_key) {
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-	p->open(slot, pin);
-	Cryptoki::KeyAttribute attr;
-	Cryptoki::MechanismInfo mInfo;
-	attr._token = TRUE;
-	mInfo._type = MT_DES2_KEY_GEN;
-	p->createKey("TestKey", attr, mInfo);
-	p->close();
-}
 
-TEST_F(CryptokiHelperTests, create_and_find_token_key) {
-	EXPECT_NO_THROW({
-		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-		std::string pin("1234");
-		unsigned long slot = 1L;
-		p->open(slot, pin);
-		Cryptoki::KeyAttribute attr;
-		Cryptoki::MechanismInfo mInfo;
-		attr._token = TRUE;
-		mInfo._type = MT_DES2_KEY_GEN;
-		p->createKey("TokenBasedTestKey", attr, mInfo);
-		p->close();
+//-----------------------------------------------------------------------------
+/**
+ * Should open and login successfully.
+ * Should not throw any exception.
+ *
+ * */
 
-		// find
-		p->open(slot, pin);
-		Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "TokenBasedTestKey");
-		k.getKcv(); // to avoid not used variable warning
-		p->close();
-	});
-}
-
-TEST_F(CryptokiHelperTests, create_and_find_session_key) {
-	EXPECT_NO_THROW({
-		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-		std::string pin("1234");
-		unsigned long slot = 1L;
-		p->open(slot, pin);
-		Cryptoki::KeyAttribute attr;
-		Cryptoki::MechanismInfo mInfo;
-		attr._token = TRUE;
-		mInfo._type = MT_DES2_KEY_GEN;
-		p->createKey("SessionBasedTestKey", attr, mInfo);
-		// find
-		Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "SessionBasedTestKey");
-		k.getKcv(); // to avoid not used variable warning
-		p->close();
-	});
-}
-
-TEST_F(CryptokiHelperTests, negative_find_key) {
-	EXPECT_THROW(
+TEST_F(CryptokiHelperTests, open_success) {
+	EXPECT_NO_THROW(
 		{
 			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-			std::string pin("1234");
+			std::string wrongPin("1234");
 			unsigned long slot = 1L;
-			p->open(slot, pin);
-			Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "TestKey");
-			k.getKcv(); // to avoid not used variable warning
+			p->open(slot, wrongPin);
 			p->close();
-		},
-		ExceptionCryptoki);
+		});
 }
 
-TEST_F(CryptokiHelperTests, negative_create_and_not_found_Session_key) {
-	EXPECT_THROW(
+//-----------------------------------------------------------------------------
+/**
+ * Should not throw any exception.
+ *
+ * */
+
+TEST_F(CryptokiHelperTests, close_twice) {
+	EXPECT_NO_THROW(
 		{
 			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-			std::string pin("1234");
+			std::string wrongPin("1234");
 			unsigned long slot = 1L;
-			p->open(slot, pin);
-			Cryptoki::KeyAttribute attr;
-			Cryptoki::MechanismInfo mInfo;
-			attr._token = FALSE;
-			mInfo._type = MT_DES2_KEY_GEN;
-			p->createKey("SessionBasedTestKey", attr, mInfo);
+			p->open(slot, wrongPin);
 			p->close();
-
-			// find
-			p->open(slot, pin);
-			Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "SessionBasedTestKey");
-			k.getKcv(); // to avoid not used variable warning
 			p->close();
-		},
-		ExceptionCryptoki);
+		});
 }
-
-// Data object
-TEST_F(CryptokiHelperTests, create_dataObject) {
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-	p->open(slot, pin);
-	unsigned char data[] = {0x01, 0x23, 0x45};
-	p->createData("TestApp", "TestData", data, sizeof(data));
-	p->close();
-}
-
-TEST_F(CryptokiHelperTests, create_and_get_dataObject) {
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-	p->open(slot, pin);
-	unsigned char data[] = {0x01, 0x23, 0x45};
-	p->createData("TestApp", "TestData", data, sizeof(data));
-	Cryptoki::DataObject d = p->getDataByName("TestApp", "TestData");
-	VectorUChar val = d.getValue();
-	p->close();
-	EXPECT_EQ(sizeof(data), val.size());
-	EXPECT_EQ(0, memcmp(data, val.data(), sizeof(data)));
-}
-
-TEST_F(CryptokiHelperTests, create_and_get_dataObject_token) {
-	char data[] = {0x01, 0x23, 0x45};
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-
-	p->open(slot, pin);
-	Cryptoki::DataAttribute attr;
-	attr._token = TRUE;
-	attr._data = data;
-	attr._dataLen = sizeof(data);
-	p->createData("TestApp", "TestData_token", attr);
-	p->close();
-
-	p->open(slot, pin);
-	Cryptoki::DataObject d = p->getDataByName("TestApp", "TestData_token");
-	VectorUChar val = d.getValue();
-	p->close();
-
-	EXPECT_EQ(sizeof(data), val.size());
-	EXPECT_EQ(0, memcmp(data, val.data(), sizeof(data)));
-}
-
-TEST_F(CryptokiHelperTests, create_and_get_dataObject_session) {
-	char data[] = {0x01, 0x23, 0x45};
-	EXPECT_THROW(
-		{
-			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-			std::string pin("1234");
-			unsigned long slot = 1L;
-
-			p->open(slot, pin);
-			Cryptoki::DataAttribute attr;
-			attr._token = FALSE;
-			attr._data = data;
-			attr._dataLen = sizeof(data);
-			p->createData("TestApp", "TestData_session", attr);
-			p->close();
-
-			p->open(slot, pin);
-			Cryptoki::DataObject d = p->getDataByName("TestApp", "TestData_session");
-			d.getValue(); // to avoid not used variable warning
-			p->close();
-		},
-		ExceptionCryptoki);
-}
-
-TEST_F(CryptokiHelperTests, get_set_dataObject) {
-	char data[] = {0x01, 0x23, 0x45};
-	char newData[] = {0x98, 0x76};
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-
-	p->open(slot, pin);
-	Cryptoki::DataAttribute attr;
-	attr._token = TRUE;
-	attr._data = data;
-	attr._dataLen = sizeof(data);
-	p->createData("TestApp", "TestData_token", attr);
-	p->close();
-
-	p->open(slot, pin);
-	Cryptoki::DataObject d = p->getDataByName("TestApp", "TestData_token");
-	d.setValue(newData, sizeof(newData));
-	VectorUChar val = d.getValue();
-	p->close();
-
-	EXPECT_EQ(sizeof(newData), val.size());
-	EXPECT_EQ(0, memcmp(newData, val.data(), sizeof(newData)));
-}
-
-TEST_F(CryptokiHelperTests, get_set_dataObject_2) {
-	char data[] = {0x01, 0x23, 0x45};
-	char newData[] = {0x98, 0x76};
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-
-	p->open(slot, pin);
-	Cryptoki::DataAttribute attr;
-	attr._token 	= TRUE;
-	attr._data 		= data;
-	attr._dataLen 	= sizeof(data);
-	p->createData("TestApp", "TestData_token", attr);
-	p->close();
-
-	p->open(slot, pin);
-	Cryptoki::DataObject d = p->getDataByName("TestApp", "TestData_token");
-	VectorUChar v;
-	v.assign(newData, newData + sizeof(newData));
-	d.setValue(v);
-	VectorUChar val = d.getValue();
-	p->close();
-
-	EXPECT_EQ(sizeof(newData), val.size());
-	EXPECT_EQ(0, memcmp(newData, val.data(), sizeof(newData)));
-}
-
-TEST_F(CryptokiHelperTests, get_dataObject_value_as_str) {
-	char data[] = {'1', '2', '3'};
-	std::string expected(data, data + sizeof(data));
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-
-	p->open(slot, pin);
-	Cryptoki::DataAttribute attr;
-	attr._token 	= TRUE;
-	attr._data 		= data;
-	attr._dataLen 	= sizeof(data);
-	p->createData("TestApp", "TestData_token_str", attr);
-	p->close();
-
-	p->open(slot, pin);
-	Cryptoki::DataObject d = p->getDataByName("TestApp", "TestData_token_str");
-	std::string val = d.getValueAsStr();
-	p->close();
-	EXPECT_EQ(0, val.compare(expected));
-}
-
-
-//TEST_F(CryptokiHelperTests, negative_closed) {
-//	EXPECT_THROW(
-//		{
-//			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-//			std::string pin("1234");
-//			unsigned long slot = 1L;
-//			p->open(slot, pin);
-//			p->close();
-//		},
-//		ExceptionCryptoki);
-//}
-//
-//
-//TEST_F(CryptokiHelperTests, negative_nonExist_DataObject) {
-//	Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
-//	//EXPECT_EQ(NULL, pC->getDataObject("GIB", "NonExistDataObject"));
-//}
-//
-//TEST_F(CryptokiHelperTests, create_DataObject) {
-//	unsigned char data[] = {0x01, 0x23};
-//	EXPECT_NO_THROW(
-//	{
-//		Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
-//		//DataObject* pData = pC->createDataObject("GIB", "Test_Data_Object", data, sizeof(data));
-//		//DataObject* pData2 = pC->getDataObject("GIB", "Test_Data_Object");
-//		//EXPECT_TRUE(pData != NULL);
-//		//EXPECT_TRUE(pData2 != NULL);
-//	});
-//}
-//
-//TEST_F(CryptokiHelperTests, getDataObject) {
-//	unsigned char data[] = {0x01, 0x23};
-//	VectorUChar expected_;
-//	expected_.assign(data, data + sizeof(data));
-//	VectorUChar retVal;
-//
-//	Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
-//	//DataObject* pData = pC->createDataObject("GIB", "Test_Data_Object", data, sizeof(data));
-//	//EXPECT_TRUE(pData != NULL);
-//	//retVal = pData->getValue();
-//
-//		//EXPECT_EQ(expected_.size(), retVal.size());
-//}
 
 #endif //_CRYPTOKI_HELPER_TESTS_H_
--- test/cryptoHelper_Tests/key_tests.h
+++ test/cryptoHelper_Tests/key_tests.h
@@ -5,6 +5,7 @@
 #include "../../src/cryptokiHelper/CryptokiHelper.h"
 #include "../../src/cryptokiHelper/Key.h"
 #include "cryptokiHelper_tests.h"
+#include "cryptokiHelperTestUtil.h"
 
 class keyTests : public ::testing::Test {
 public:
@@ -13,40 +14,152 @@ public:
 
     virtual void TearDown() {
     	std::string pin("1234");
-		CryptokiHelperTests::CryptokiHelperEx::ClearSlot(1L, pin);
+    	CryptokiHelperTestUtil::ClearSlot(1L, pin);
     }
 };
 
+//-----------------------------------------------------------------------------
+/**
+ * Creates a DES2 Key
+ * */
+
+TEST_F(keyTests, create_key) {
+	EXPECT_NO_THROW({
+		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+		std::string pin("1234");
+		unsigned long slot = 1L;
+		p->open(slot, pin);
+		Cryptoki::KeyAttribute attr;
+		Cryptoki::MechanismInfo mInfo;
+		attr._token = TRUE;
+		attr._class = CKO_SECRET_KEY;
+		mInfo._type = MT_DES2_KEY_GEN;
+		p->createKey("TestKey", attr, mInfo);
+		p->close();
+	});
+}
+
+//-----------------------------------------------------------------------------
+
+TEST_F(keyTests, create_and_find_token_key) {
+//	EXPECT_NO_THROW({
+		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+		std::string pin("1234");
+		unsigned long slot = 1L;
+		p->open(slot, pin);
+		Cryptoki::KeyAttribute attr;
+		Cryptoki::MechanismInfo mInfo;
+		attr._token = TRUE;
+		attr._class = OC_SECRET_KEY;
+		mInfo._type = MT_DES2_KEY_GEN;
+		p->createKey("TokenBasedTestKey", attr, mInfo);
+		p->close();
+
+		// find
+		p->open(slot, pin);
+		Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "TokenBasedTestKey");
+		k.getKcv(MT_DES3_ECB); // to avoid from not used variable warning
+		p->close();
+//	});
+}
+
+//-----------------------------------------------------------------------------
+
+TEST_F(keyTests, create_and_find_session_key) {
+	EXPECT_NO_THROW({
+		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+		std::string pin("1234");
+		unsigned long slot = 1L;
+		p->open(slot, pin);
+		Cryptoki::KeyAttribute attr;
+		Cryptoki::MechanismInfo mInfo;
+		attr._token = TRUE;
+		mInfo._type = MT_DES2_KEY_GEN;
+		p->createKey("SessionBasedTestKey", attr, mInfo);
+		// find
+		Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "SessionBasedTestKey");
+		k.getKcv(MT_DES3_ECB); // to avoid from not used variable warning
+		p->close();
+	});
+}
+
+//-----------------------------------------------------------------------------
+
+TEST_F(keyTests, negative_find_key) {
+	EXPECT_THROW(
+		{
+			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+			std::string pin("1234");
+			unsigned long slot = 1L;
+			p->open(slot, pin);
+			Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "TestKey"); // Throw here!
+			k.getKcv(); // to avoid from not used variable warning
+			p->close();
+		},
+		ExceptionCryptoki);
+}
+
+//-----------------------------------------------------------------------------
+
+TEST_F(keyTests, negative_create_and_not_found_Session_key) {
+	EXPECT_THROW(
+		{
+			Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+			std::string pin("1234");
+			unsigned long slot = 1L;
+			p->open(slot, pin);
+			Cryptoki::KeyAttribute attr;
+			Cryptoki::MechanismInfo mInfo;
+			attr._token = FALSE;
+			mInfo._type = MT_DES2_KEY_GEN;
+			p->createKey("SessionBasedTestKey", attr, mInfo);
+			p->close();
+
+			// find
+			p->open(slot, pin);
+			Cryptoki::Key k = p->getKeyByName(OC_SECRET_KEY, "SessionBasedTestKey"); // Throws here!
+			k.getKcv(); // to avoid from not used variable warning
+			p->close();
+		},
+		ExceptionCryptoki);
+}
+
+//-----------------------------------------------------------------------------
+
 TEST_F(keyTests, enc_dec) {
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-	p->open(slot, pin);
-
-	Cryptoki::KeyAttribute attr;
-	attr._token = TRUE;
-
-	Cryptoki::MechanismInfo mInfo;
-	mInfo._type = MT_DES2_KEY_GEN;
-	Cryptoki::Key k = p->createKey("SessionBasedTestKey_for_encdec", attr, mInfo);
-
-	unsigned char iv[8] = {0x00};
-	mInfo._type = MT_DES3_CBC;
-	mInfo._param = iv;
-	mInfo._paramLen = sizeof(iv);
 	char clearData[] = {
 						0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 						0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17
 						};
-	VectorUChar encData = k.encrypt(mInfo, clearData, sizeof(clearData));
-	VectorUChar decData = k.decrypt(mInfo, encData);
+	EXPECT_NO_THROW({
+		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+		std::string pin("1234");
+		unsigned long slot = 1L;
+		p->open(slot, pin);
 
-	p->close();
-	EXPECT_TRUE(encData.size() != 0);
-	EXPECT_TRUE(decData.size() != 0);
-	EXPECT_EQ(sizeof(clearData), decData.size());
-	EXPECT_EQ(0, memcmp(clearData, decData.data(), decData.size()));
+		Cryptoki::KeyAttribute attr;
+		attr._token = TRUE;
+
+		Cryptoki::MechanismInfo mInfo;
+		mInfo._type = MT_DES2_KEY_GEN;
+		Cryptoki::Key k = p->createKey("SessionBasedTestKey_for_encdec", attr, mInfo);
+
+		unsigned char iv[8] = {0x00};
+		mInfo._type = MT_DES3_CBC;
+		mInfo._param = iv;
+		mInfo._paramLen = sizeof(iv);
+		VectorUChar encData = k.encrypt(mInfo, clearData, sizeof(clearData));
+		VectorUChar decData = k.decrypt(mInfo, encData);
+
+		p->close();
+		EXPECT_TRUE(encData.size() != 0);
+		EXPECT_TRUE(decData.size() != 0);
+		EXPECT_EQ(sizeof(clearData), decData.size());
+		EXPECT_EQ(0, memcmp(clearData, decData.data(), decData.size()));
+	});
 }
+
+//-----------------------------------------------------------------------------
 
 TEST_F(keyTests, negative_encrypt_not_permitted) {
 	char clearData[] = {
@@ -84,6 +197,8 @@ TEST_F(keyTests, negative_encrypt_not_permitted) {
 		ExceptionCryptoki);
 }
 
+//-----------------------------------------------------------------------------
+
 TEST_F(keyTests, negative_decrypt_not_permitted) {
 	char clearData[] = {
 						0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
@@ -109,7 +224,7 @@ TEST_F(keyTests, negative_decrypt_not_permitted) {
 			mInfo._param = iv;
 			mInfo._paramLen = sizeof(iv);
 			VectorUChar encData = k.encrypt(mInfo, clearData, sizeof(clearData));
-			VectorUChar decData = k.decrypt(mInfo, encData);
+			VectorUChar decData = k.decrypt(mInfo, encData); // Throws here!
 
 			p->close();
 			EXPECT_TRUE(encData.size() != 0);
@@ -120,42 +235,48 @@ TEST_F(keyTests, negative_decrypt_not_permitted) {
 		ExceptionCryptoki);
 }
 
+//-----------------------------------------------------------------------------
+
 TEST_F(keyTests, wrap_unwrap) {
-	Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
-	std::string pin("1234");
-	unsigned long slot = 1L;
-	p->open(slot, pin);
-
-	Cryptoki::MechanismInfo mInfo;
-	mInfo._type = MT_DES2_KEY_GEN;
-	Cryptoki::Key kWrapper = p->createKey("Test_WrapperKey", mInfo);
-	Cryptoki::Key k = p->createKey("Test_Key", mInfo);
-
-	unsigned char iv[8] = {0x00};
-	mInfo._type = MT_DES3_CBC;
-	mInfo._param = iv;
-	mInfo._paramLen = sizeof(iv);
-	VectorUChar wrappedKey = kWrapper.wrap(mInfo, k);
-	Cryptoki::KeyAttribute attr;
-	attr._keyType = KT_DES2;
-	Cryptoki::Key kTarget = kWrapper.unwrap(mInfo, wrappedKey, attr);
-
-	mInfo._type = MT_DES3_CBC;
-	mInfo._param = iv;
-	mInfo._paramLen = sizeof(iv);
 	char clearData[] = {
 						0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 						0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17
 						};
-	VectorUChar encData = k.encrypt(mInfo, clearData, sizeof(clearData));
-	VectorUChar decData = kTarget.decrypt(mInfo, encData);
+	EXPECT_NO_THROW({
+		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+		std::string pin("1234");
+		unsigned long slot = 1L;
+		p->open(slot, pin);
 
-	p->close();
-	EXPECT_TRUE(encData.size() != 0);
-	EXPECT_TRUE(decData.size() != 0);
-	EXPECT_EQ(sizeof(clearData), decData.size());
-	EXPECT_EQ(0, memcmp(clearData, decData.data(), decData.size()));
+		Cryptoki::MechanismInfo mInfo;
+		mInfo._type = MT_DES2_KEY_GEN;
+		Cryptoki::Key kWrapper = p->createKey("Test_WrapperKey", mInfo);
+		Cryptoki::Key k = p->createKey("Test_Key", mInfo);
+
+		unsigned char iv[8] = {0x00};
+		mInfo._type = MT_DES3_CBC;
+		mInfo._param = iv;
+		mInfo._paramLen = sizeof(iv);
+		VectorUChar wrappedKey = kWrapper.wrap(mInfo, k);
+		Cryptoki::KeyAttribute attr;
+		attr._keyType = KT_DES2;
+		Cryptoki::Key kTarget = kWrapper.unwrap(mInfo, wrappedKey, attr);
+
+		mInfo._type = MT_DES3_CBC;
+		mInfo._param = iv;
+		mInfo._paramLen = sizeof(iv);
+		VectorUChar encData = k.encrypt(mInfo, clearData, sizeof(clearData));
+		VectorUChar decData = kTarget.decrypt(mInfo, encData);
+
+		p->close();
+		EXPECT_TRUE(encData.size() != 0);
+		EXPECT_TRUE(decData.size() != 0);
+		EXPECT_EQ(sizeof(clearData), decData.size());
+		EXPECT_EQ(0, memcmp(clearData, decData.data(), decData.size()));
+	});
 }
+
+//-----------------------------------------------------------------------------
 
 TEST_F(keyTests, negative_wrap_not_permitted) {
 	unsigned char iv[8] = {0x00};
@@ -180,7 +301,7 @@ TEST_F(keyTests, negative_wrap_not_permitted) {
 				mInfo._type = MT_DES3_CBC;
 				mInfo._param = iv;
 				mInfo._paramLen = sizeof(iv);
-				VectorUChar wrappedKey = kWrapper.wrap(mInfo, k);
+				VectorUChar wrappedKey = kWrapper.wrap(mInfo, k); // Throws here
 				Cryptoki::KeyAttribute attr;
 				attr._keyType = KT_DES2;
 				Cryptoki::Key kTarget = kWrapper.unwrap(mInfo, wrappedKey, attr);
@@ -199,6 +320,8 @@ TEST_F(keyTests, negative_wrap_not_permitted) {
 			},
 			ExceptionCryptoki);
 }
+
+//-----------------------------------------------------------------------------
 
 TEST_F(keyTests, negative_unwrap_not_permitted) {
 	unsigned char iv[8] = {0x00};
@@ -227,7 +350,7 @@ TEST_F(keyTests, negative_unwrap_not_permitted) {
 				VectorUChar wrappedKey = kWrapper.wrap(mInfo, k);
 				Cryptoki::KeyAttribute attr;
 				attr._keyType = KT_DES2;
-				Cryptoki::Key kTarget = kWrapper.unwrap(mInfo, wrappedKey, attr);
+				Cryptoki::Key kTarget = kWrapper.unwrap(mInfo, wrappedKey, attr); // Throws here!
 
 				mInfo._type = MT_DES3_CBC;
 				mInfo._param = iv;
@@ -242,6 +365,35 @@ TEST_F(keyTests, negative_unwrap_not_permitted) {
 				EXPECT_EQ(0, memcmp(clearData, decData.data(), decData.size()));
 			},
 			ExceptionCryptoki);
+}
+
+//-----------------------------------------------------------------------------
+
+TEST_F(keyTests, create_key_by_value) {
+	char keyVal[32];
+	EXPECT_NO_THROW({
+		std::string keyName("Test_AES_by_val");
+		Cryptoki::CryptokiHelper* p = Cryptoki::CryptokiHelper::instance();
+		std::string pin("1234");
+		unsigned long slot = 1L;
+		p->open(slot, pin);
+
+		Cryptoki::MechanismInfo mInfo;
+		mInfo._param 	= keyVal;
+		mInfo._paramLen = sizeof(keyVal);
+
+		Cryptoki::KeyAttribute kAttr;
+		kAttr._label 	= keyName;
+		kAttr._keyType 	= KT_AES;
+		Cryptoki::Key k = p->createSecretKey(keyName, kAttr, mInfo);
+		VectorUChar kcv = k.getKcv();
+
+		Cryptoki::Key kk = p->getKeyByName(OC_SECRET_KEY, keyName);
+		VectorUChar kkcv = kk.getKcv();
+		p->close();
+
+		EXPECT_EQ(kcv, kkcv);
+	});
 }
 
 #endif// _KEY_TESTS_H_
