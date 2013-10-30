97f082ea66487ff07ee7be52da3a8e4b4f64ad1e
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-30 11:08:42 +0200

    Session setup() da aciliyor. Her tesste kullaniliyor.

--- test/safenetHelper_tests.h
+++ test/safenetHelper_tests.h
@@ -24,61 +24,65 @@
 
 class SafenetHelperTests : public ::testing::Test {
 public:
-    virtual void SetUp() {
+	SafenetHelperTests() :
+		_pin("1234"),
+		_slot(1L)
+	{
+		_pSafenet = SafenetHelper::instance();
+		_pSafenet->login(_slot, _pin);
+	}
+	virtual ~SafenetHelperTests() {
+    	CryptokiHelperTestUtil::ClearSlot(_slot, _pin);
+	}
+
+	virtual void SetUp() {
     }
 
     virtual void TearDown() {
-    	std::string pin("1234");
-    	CryptokiHelperTestUtil::ClearSlot(1L, pin);
+    	//std::string pin("1234");
+    	//CryptokiHelperTestUtil::ClearSlot(1L, pin);
     }
+
+protected:
+    SafenetHelper* 	_pSafenet;
+    std::string 	_pin;
+    int 			_slot;
 };
 
 //-----------------------------------------------------------------------
 
 TEST_F(SafenetHelperTests, negative_you_cannot_login_into_nonexistent_slot) {
 	unsigned long nonExistentSlot = 9999L;
-	std::string pin("1234");
-	SafenetHelper* pS = SafenetHelper::instance();
 
 	EXPECT_THROW({
-		pS->login(nonExistentSlot, pin);
+		_pSafenet->login(nonExistentSlot, _pin);
 	}, ExceptionCryptoki);
 }
 
 //-----------------------------------------------------------------------
 
 TEST_F(SafenetHelperTests, negative_wrong_pin_should_throw_exception) {
-	unsigned long nonExistentSlot = 1L;
-	std::string pin("9999");
-	SafenetHelper* pS = SafenetHelper::instance();
+	std::string wrongPin("9999");
 
 	EXPECT_THROW({
-		pS->login(nonExistentSlot, pin);
+		_pSafenet->login(_slot, wrongPin);
 	}, ExceptionCryptoki);
 }
 
 //-----------------------------------------------------------------------
 
 TEST_F(SafenetHelperTests, should_login) {
-	unsigned long existentSlot = 1L;
-	std::string pin("1234");
-	SafenetHelper* pS = SafenetHelper::instance();
-
 	EXPECT_NO_THROW({
-		pS->login(existentSlot, pin);
+		_pSafenet->login(_slot, _pin);
 	});
 }
 
 //-----------------------------------------------------------------------
 
 TEST_F(SafenetHelperTests, can_be_relogin) {
-	unsigned long existentSlot = 1L;
-	std::string pin("1234");
-	SafenetHelper* pS = SafenetHelper::instance();
-
 	EXPECT_NO_THROW({
-		pS->login(existentSlot, pin);
-		pS->login(existentSlot, pin);
+		_pSafenet->login(_slot, _pin);
+		_pSafenet->login(_slot, _pin);
 	});
 }
 
@@ -89,12 +93,7 @@ TEST_F(SafenetHelperTests, create_key_token) {
 		VectorUChar outKey;
 		VectorUChar outKcv;
 		int err;
-		std::string pin("1234");
-
-		SafenetHelper* pS = SafenetHelper::instance();
-		pS->login(1L, pin);
-		pS->setup();
-		err = pS->GenerateAES256Key(outKey, outKcv);
+		err = _pSafenet->GenerateAES256Key(outKey, outKcv);
 
 		EXPECT_EQ(SUCCESS, err);
 		//EXPECT_TRUE(outKey.size() != 0);
@@ -106,23 +105,20 @@ TEST_F(SafenetHelperTests, create_key_token) {
 
 TEST_F(SafenetHelperTests, setup) {
 	EXPECT_NO_THROW({
-		int err;
-		std::string pin("1234");
-
-		SafenetHelper* pS = SafenetHelper::instance();
-		pS->login(1L, pin);
-		err = pS->setup();
-
-		EXPECT_EQ(SUCCESS, err);
-
 		Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
-		Cryptoki::DataObject d = pC->getDataByName("GIB", "ActiveLmkIndex");
-		Cryptoki::Key k =  pC->getKeyByName(OC_SECRET_KEY, "LMK_000");
-		std::string data = d.getValueAsStr();
-		k.getKcv(MT_DES3_ECB); // To avoid get warning message
 
-		std::string expectedIndexData("0000");
-		EXPECT_EQ(0, data.compare(expectedIndexData));
+		Cryptoki::DataObject d = pC->getDataByName("GIB", "ActiveLmkIndex");
+		std::string data = d.getValueAsStr();
+		//std::string expectedIndexData("0000");
+		//EXPECT_EQ(0, data.compare(expectedIndexData));
+
+
+		Cryptoki::Key k 			= pC->getKeyByName(OC_SECRET_KEY,  "LMK_000");
+		Cryptoki::Key publicKey 	= pC->getKeyByName(OC_PUBLIC_KEY,  "PbK_GIB");
+		Cryptoki::Key privateKey 	= pC->getKeyByName(OC_PRIVATE_KEY, "PrK_GIB");
+
+
+		k.getKcv(MT_DES3_ECB); // To avoid get warning message
 	});
 }
 
@@ -131,15 +127,9 @@ TEST_F(SafenetHelperTests, setup) {
 TEST_F(SafenetHelperTests, addLmk) {
 	EXPECT_NO_THROW({
 		int err;
-		std::string pin("1234");
 		std::string expectedData;
 
-		SafenetHelper* pS = SafenetHelper::instance();
-		pS->login(1L, pin);
-		err = pS->setup();
-		EXPECT_EQ(SUCCESS, err);
-
-		err = pS->addLmk();
+		err = _pSafenet->addLmk();
 		EXPECT_EQ(SUCCESS, err);
 
 		Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
@@ -151,7 +141,7 @@ TEST_F(SafenetHelperTests, addLmk) {
 //		k.getKcv();
 //
 		// add again
-		err = pS->addLmk();
+		err = _pSafenet->addLmk();
 		EXPECT_EQ(SUCCESS, err);
 		d = pC->getDataByName("GIB", "ActiveLmkIndex");
 		dataValue = d.getValueAsStr();
@@ -160,7 +150,7 @@ TEST_F(SafenetHelperTests, addLmk) {
 		k =  pC->getKeyByName(OC_SECRET_KEY, "LMK_002");
 
 		// add again
-		err = pS->addLmk();
+		err = _pSafenet->addLmk();
 		EXPECT_EQ(SUCCESS, err);
 		d = pC->getDataByName("GIB", "ActiveLmkIndex");
 		dataValue = d.getValueAsStr();
@@ -169,7 +159,29 @@ TEST_F(SafenetHelperTests, addLmk) {
 		k =  pC->getKeyByName(OC_SECRET_KEY, "LMK_003");
 	});
 }
+
 //-----------------------------------------------------------------------
+
+TEST_F(SafenetHelperTests, getFisCalNo) {
+	EXPECT_NO_THROW({
+//		std::string pin("1234");
+//		SafenetHelper* pS = SafenetHelper::instance();
+//		Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
+//		Cryptoki::Key pubKey = pC->getKeyByName(OC_PUBLIC_KEY, "PbK_CAP");
+//
+//		pS->login(1L, pin);
+//		int err = pS->setup();
+//
+//		// 1. Pg ile enc edilerek PgFisCalNo bulunur.
+//		// 2. getFisCalNo()'ya bu verilir
+//		// 3. Sonucta fsCalNo beklenir.
+//
+//		char PgFisCalNo[256]; // calculate!
+//		char fisCalNo[256];
+//		err = pS->getFisCalNo(PgFisCalNo, fisCalNo);
+//		EXPECT_EQ(SUCCESS, err);
+	});
+}
 //-----------------------------------------------------------------------
 //-----------------------------------------------------------------------
 //-----------------------------------------------------------------------
