015cbe4cfc4ef2dcf85612b85e386e9c59a91221
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-30 16:40:56 +0200

    Cryptoki ile bussiness katmanı arasında yapılacak genel işleri bulunduran sınıftır.

---
+++ src/SafenetHelperUtil.h
@@ -0,0 +1,19 @@
+//---------------------------------------------------------
+// Mehmet Suyuti Dindar
+// 30.10.2013
+// SmartSoft
+//---------------------------------------------------------
+
+#ifndef _SAFENET_HELPER_UTIL_H_
+#define _SAFENET_HELPER_UTIL_H_
+
+#include "cryptokiHelper/Key.h"
+#include "cryptokiHelper/CryptokiHelper.h"
+
+class SafenetHelperUtil {
+public:
+	static Cryptoki::Key getActiveLmk(Cryptoki::CryptokiHelper& session);
+	static int getActiveLmkIndex(Cryptoki::CryptokiHelper& session);
+};
+
+#endif //_SAFENET_HELPER_UTIL_H_
