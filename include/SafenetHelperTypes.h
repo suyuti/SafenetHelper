afe90b2b1ba0e2323fc5c9797b9479a5ac5fa685
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-30 12:00:12 +0200

    interface de kullanilacak veri yapilari eklendi.

--- include/SafenetHelperTypes.h
+++ include/SafenetHelperTypes.h
@@ -12,5 +12,34 @@
 using namespace std;
 
 typedef std::vector<unsigned char> VectorUChar;
+typedef std::vector<char> VectorChar;
+
+typedef struct {
+	long 		_lmkIndex;
+	VectorUChar	_lmk_TRAK;
+	VectorUChar	_lmk_TREK;
+	VectorUChar	_kcv_TRAK;
+	VectorUChar	_kcv_TREK;
+	VectorUChar	_TRMK_TRAK;
+	VectorUChar	_TRMK_TREK;
+	VectorUChar	_Signature;
+} KeyExchangeResponse;
+
+typedef struct {
+	long 		_lmkIndex;
+	VectorUChar	_lmk_TRAK;
+	VectorUChar	_lmk_TREK;
+	VectorUChar	_kcv_TRAK;
+	VectorUChar	_kcv_TREK;
+	VectorUChar _sha256Data;
+	VectorUChar _trek_data;
+	VectorUChar _trak_sha256Data;
+} ProcessRequest;
+
+typedef struct {
+	VectorUChar _data;
+	VectorUChar _trak_sha256Data;
+	VectorUChar _trek_data;
+} ProcessResponse;
 
 #endif //_SAFENET_HELPER_TYPES_H_
