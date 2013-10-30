6f03ec8b29b8e57890f8c9163ea361f062a274f0
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-10-30 11:06:56 +0200

    HSM kurulduktan sonra yasamasi gereken objeler silinmiyor. ActiveLmkIndex, LMK_XXX, Pr, Pb, ClearSlot() son parametresi tamamini sil olarak eklendi.

--- test/cryptoHelper_Tests/cryptokiHelperTestUtil.h
+++ test/cryptoHelper_Tests/cryptokiHelperTestUtil.h
@@ -4,12 +4,13 @@
 #include <stdio.h>
 #include "cryptoki.h"
 #include "../../src/cryptokiHelper/CryptokiHelper.h"
+#include <string.h>
 
 
 class CryptokiHelperTestUtil
 {
 public:
-	inline static void ClearSlot(unsigned long slotId, std::string& pin) {
+	inline static void ClearSlot(unsigned long slotId, std::string pin, bool clearAll = false) {
 		Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
 		pC->close();
 		pC->open(slotId, pin);
@@ -38,7 +39,37 @@ public:
 	    }
 
 		for (CK_ULONG i = 0; i < numObjectsFound; ++i) {
-			C_DestroyObject(pC->getSessionHandle(), h[i]);
+			if (clearAll) {
+				C_DestroyObject(pC->getSessionHandle(), h[i]);
+			}
+			else {
+				CK_ATTRIBUTE cmkTemplate[] = {{CKA_LABEL, NULL, 0}};
+				char label[256];
+				rv = C_GetAttributeValue(pC->getSessionHandle(), h[i], cmkTemplate, 1); // Sadece attribute size öğrenmek için
+				if (rv != CKR_OK) {
+					throw ExceptionCryptoki(rv, __FILE__, __LINE__);
+				}
+				cmkTemplate[0].pValue = (CK_BYTE_PTR) malloc(cmkTemplate[0].valueLen + 1);
+				memset(cmkTemplate[0].pValue, 0x00, (cmkTemplate[0].valueLen + 1));
+
+				rv = C_GetAttributeValue(pC->getSessionHandle(), h[i], cmkTemplate, 1);
+				if (rv != CKR_OK) {
+					delete(cmkTemplate[0].pValue);
+					throw ExceptionCryptoki(rv, __FILE__, __LINE__);
+				}
+
+				if ((strncmp((char*)cmkTemplate[0].pValue, "ActiveLmkIndex", strlen("ActiveLmkIndex")) 	== 0) ||
+					(strncmp((char*)cmkTemplate[0].pValue, "LMK_", 			strlen("LMK_")) 			== 0) ||
+					(strncmp((char*)cmkTemplate[0].pValue, "PbK_GIB", 		strlen("PbK_GIB")) 			== 0) ||
+					(strncmp((char*)cmkTemplate[0].pValue, "PrK_GIB", 		strlen("PrK_GIB")) 			== 0)
+					) {
+					// Bunlari silme
+				}
+				else {
+					C_DestroyObject(pC->getSessionHandle(), h[i]);
+				}
+				delete(cmkTemplate[0].pValue);
+			}
 		}
 		pC->close();
 	};
