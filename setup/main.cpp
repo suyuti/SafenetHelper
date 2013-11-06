66c79206747064adbf1ee987d81e11e7d55f5961
Faik Uygur <faik.uygur@cardtek.com>
2013-11-06 16:29:12 +0200

    menu skeleton with all commands

--- setup/main.cpp
+++ setup/main.cpp
@@ -27,6 +27,48 @@ void sleep(int seconds)
 #endif
 }
 
+// 1. Setup
+bool doSetup()
+{
+	return false;
+}
+
+// 2.  Add LMK
+bool doAddLMK()
+{
+	return false;
+}
+
+// 3.  Get Active LMK
+bool doGetActiveLMK()
+{
+	return false;
+}
+
+// 4.  Get KCV
+bool doGetKCV()
+{
+	return false;
+}
+
+// 5.  Export Ps
+bool doExportPs()
+{
+	return false;
+}
+
+// 6.  Backup
+bool doBackup()
+{
+	return false;
+}
+
+// 7.  Restore
+bool doRestore()
+{
+	return false;
+}
+
 int main(int argc, char **argv)
 {
     char selection = 0;
@@ -38,10 +80,12 @@ int main(int argc, char **argv)
 	    cout << "  ===========================\n";
 	    cout << "  1.  Setup\n";
 	    cout << "  2.  Add LMK\n";
-	    cout << "  3.  Export Ps\n";
-	    cout << "  4.  Backup\n";
-	    cout << "  5.  Restore\n";
-	    cout << "  6.  Exit\n";
+	    cout << "  3.  Get Active LMK\n";
+	    cout << "  4.  Get KCV\n";
+	    cout << "  5.  Export Ps\n";
+	    cout << "  6.  Backup\n";
+	    cout << "  7.  Restore\n";
+	    cout << "  8.  Exit\n";
 	    cout << "  ===========================\n";
 	    cout << "  Enter your selection: ";
 	    cin >> selection;
@@ -49,26 +93,42 @@ int main(int argc, char **argv)
 
 	    switch (selection) {
 		case '1':
-		    cout << "Setup\n";
+		    if (doSetup())
+			    cout << "Setup success..\n";
+		    else
+			    cout << "Setup failed..\n";
 		    cout << "\n";
 		    break;
 
 		case '2':
-		    cout << "Add LMK\n";
+		    if (doAddLMK())
+			    cout << "Add LMK success..\n";
+		    else
+			    cout << "Add LMK failed..\n";
 		    cout << "\n";
 		    break;
 		case '3':
-		    cout << "Export Ps\n" ;
+		    if (doExportPs())
+			    cout << "Export Ps success..\n";
+		    else
+			    cout << "Export Ps failed..\n";
 		    cout << "\n";
 		    break;
 
 		case '4':
-		    cout << "Backup\n";
+		    if (doBackup())
+			    cout << "Backup success..\n";
+		    else
+			    cout << "Backup failed..\n";
 		    cout << "\n";
 		    break;
 
 		case '5':
-		    cout << "Restore\n";
+		    if (doRestore())
+			    cout << "Restore success..\n";
+		    else
+			    cout << "Restore failed..\n";
+		    cout << "\n";
 		    break;
 
 		case '6':
