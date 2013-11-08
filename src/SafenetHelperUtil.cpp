a5af4144922440b51133bab10486e4738feee656
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-11-08 15:13:49 +0200

    Key üretim metodları eklendi.

--- src/SafenetHelperUtil.cpp
+++ src/SafenetHelperUtil.cpp
@@ -24,6 +24,7 @@ log4cxx::LoggerPtr g_logger(log4cxx::Logger::getLogger("main"));
 log4cxx::LoggerPtr g_loggerCryptoki(log4cxx::Logger::getLogger("Cryptoki"));
 log4cxx::LoggerPtr g_loggerKey(log4cxx::Logger::getLogger("Key"));
 log4cxx::LoggerPtr g_loggerDataObject(log4cxx::Logger::getLogger("DataObject"));
+log4cxx::LoggerPtr g_loggerTest(log4cxx::Logger::getLogger("Test"));
 
 
 Cryptoki::Key SafenetHelperUtil::getActiveLmk(Cryptoki::CryptokiHelper& session)
@@ -48,4 +49,90 @@ Cryptoki::Key SafenetHelperUtil::getLmk(Cryptoki::CryptokiHelper& session, int l
 	ss << "LMK_" << setfill('0') << setw(3) << lmkIndex;
 	LOG4CXX_DEBUG(g_logger, "GetLmk: " << ss.str());
 	return session.getKeyByName(OC_SECRET_KEY, ss.str());
+}
+
+Cryptoki::Key SafenetHelperUtil::createDES2Key(Cryptoki::CryptokiHelper* pSession, std::string keyName, Cryptoki::KeyAttribute attr)
+{
+	Cryptoki::Key 			key;
+
+	Cryptoki::MechanismInfo mInfo;
+	mInfo._type = MT_DES2_KEY_GEN;
+
+	Cryptoki::KeyAttribute kAttr;
+	kAttr._decrypt 		= TRUE & attr._decrypt;
+	kAttr._encrypt 		= TRUE & attr._encrypt;
+	kAttr._extractable 	= TRUE & attr._extractable;
+	kAttr._keyType 		= KT_DES2;
+	kAttr._private 		= TRUE & attr._private;
+	kAttr._sensitive 	= TRUE & attr._sensitive;
+	kAttr._token 		= TRUE & attr._token;
+	kAttr._unwrap 		= TRUE & attr._unwrap;
+	kAttr._wrap 		= TRUE & attr._wrap;
+	kAttr._label		= attr._label.size() == 0 ?
+													keyName :
+													attr._label;
+
+	key = pSession->createKey(keyName, kAttr, mInfo);
+	return key;
+}
+
+
+Cryptoki::Key SafenetHelperUtil::createDES2Key(Cryptoki::CryptokiHelper* pSession, std::string keyName)
+{
+	Cryptoki::KeyAttribute kAttr;
+	kAttr._decrypt 		= TRUE;
+	kAttr._encrypt 		= TRUE;
+	kAttr._extractable 	= TRUE;
+	kAttr._keyType 		= KT_DES2;
+	kAttr._private 		= TRUE;
+	kAttr._sensitive 	= TRUE;
+	kAttr._token 		= TRUE;
+	kAttr._unwrap 		= TRUE;
+	kAttr._wrap 		= TRUE;
+	kAttr._label		= keyName;
+
+	return createDES2Key(pSession, keyName, kAttr);
+}
+
+Cryptoki::Key SafenetHelperUtil::createAES256Key(Cryptoki::CryptokiHelper* pSession, std::string keyName)
+{
+	Cryptoki::KeyAttribute kAttr;
+	kAttr._decrypt 		= TRUE;
+	kAttr._encrypt 		= TRUE;
+	kAttr._extractable 	= TRUE;
+	kAttr._keyType 		= KT_AES;
+	kAttr._private 		= TRUE;
+	kAttr._sensitive 	= TRUE;
+	kAttr._token 		= TRUE;
+	kAttr._unwrap 		= TRUE;
+	kAttr._wrap 		= TRUE;
+	kAttr._label		= keyName;
+
+	return createAES256Key(pSession, keyName, kAttr);
+}
+
+Cryptoki::Key SafenetHelperUtil::createAES256Key(Cryptoki::CryptokiHelper* pSession, std::string keyName, Cryptoki::KeyAttribute attr)
+{
+	Cryptoki::MechanismInfo mInfo;
+	char 					iv[32];
+
+	mInfo._type 		= MT_AES_KEY_GEN;
+	mInfo._param 		= iv;
+	mInfo._paramLen 	= sizeof(iv);
+
+	Cryptoki::KeyAttribute kAttr;
+	kAttr._decrypt 		= TRUE & attr._decrypt;
+	kAttr._encrypt 		= TRUE & attr._encrypt;
+	kAttr._extractable 	= TRUE & attr._extractable;
+	kAttr._keyType 		= KT_AES;
+	kAttr._private 		= TRUE & attr._private;
+	kAttr._sensitive 	= TRUE & attr._sensitive;
+	kAttr._token 		= TRUE & attr._token;
+	kAttr._unwrap 		= TRUE & attr._unwrap;
+	kAttr._wrap 		= TRUE & attr._wrap;
+	kAttr._label		= attr._label.size() == 0 ?
+													keyName :
+													attr._label;
+
+	return pSession->createSecretKey(keyName, kAttr, mInfo);
 }
--- src/SafenetHelperUtil.h
+++ src/SafenetHelperUtil.h
@@ -15,6 +15,16 @@ public:
 	static Cryptoki::Key getActiveLmk(Cryptoki::CryptokiHelper& session);
 	static int getActiveLmkIndex(Cryptoki::CryptokiHelper& session);
 	static Cryptoki::Key getLmk(Cryptoki::CryptokiHelper& session, int lmkIndex);
+	static Cryptoki::Key createDES2Key(	  Cryptoki::CryptokiHelper* pSession,
+									   	  std::string keyName);
+	static Cryptoki::Key createDES2Key(	  Cryptoki::CryptokiHelper* pSession,
+									   	  std::string keyName,
+									   	  Cryptoki::KeyAttribute attr);
+	static Cryptoki::Key createAES256Key( Cryptoki::CryptokiHelper* pSession,
+									  	  std::string keyName);
+	static Cryptoki::Key createAES256Key( Cryptoki::CryptokiHelper* pSession,
+									  	  std::string keyName,
+									   	  Cryptoki::KeyAttribute attr);
 };
 
 #endif //_SAFENET_HELPER_UTIL_H_
