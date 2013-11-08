52a73d80675a0a04fd88e2f6476f5a8d7cf3d8f8
Mehmet Suyuti Dindar <mehmet.dindar@cardtek.com>
2013-11-08 16:09:54 +0200

    negative_processFirst_invalid_kcv_of_trek testi eklendi

--- test/safenetHelper_tests.h
+++ test/safenetHelper_tests.h
@@ -427,6 +427,61 @@ TEST_F(SafenetHelperTests, negative_processFirst_invalid_kcv_of_trak) {
 	}, ExceptionCryptoki);
 }
 
+TEST_F(SafenetHelperTests, negative_processFirst_invalid_kcv_of_trek) {
+	char _data[] = {
+			'T','H','I','S',' ',
+			'I','S',' ',
+			'T','E','S','T',' ',
+			'D','A','T','A',' ',
+			'N','O','T',' ',
+			'S','P','A','R','T','A',' ',' ',' ',' '
+	};
+	LOG4CXX_INFO(g_loggerTest, "Test: negative_processFirst_invalid_kcv_of_trek");
+	EXPECT_THROW({
+		// test preperation
+		Cryptoki::CryptokiHelper* pC = Cryptoki::CryptokiHelper::instance();
+		VectorUChar data;
+		data.assign(_data, _data + sizeof(_data));
+
+		ProcessFirstRequest req;
+		Cryptoki::Key lmk = SafenetHelperUtil::getActiveLmk(*pC);
+
+		Cryptoki::KeyAttribute kAttr;
+		kAttr._token 	= FALSE;
+		Cryptoki::Key trak = SafenetHelperUtil::createAES256Key(pC, "TRAK", kAttr);
+		Cryptoki::Key trek = SafenetHelperUtil::createAES256Key(pC, "TREK", kAttr);
+
+		char keyVal[32];
+		Cryptoki::MechanismInfo mInfo;
+		mInfo._param 	= keyVal;
+		mInfo._paramLen = sizeof(keyVal);
+
+		req._lmkIndex 	= SafenetHelperUtil::getActiveLmkIndex(*pC);
+		mInfo._param 	= NULL;
+		mInfo._paramLen = 0;
+		mInfo._type 	= MT_DES3_ECB;
+		req._lmk_TRAK	= lmk.wrap(mInfo, trak);
+		req._lmk_TREK	= lmk.wrap(mInfo, trek);
+		req._kcv_TRAK 	= trak.getKcv();
+		req._kcv_TREK 	= trek.getKcv();
+
+		std::rotate(req._kcv_TREK.begin(), req._kcv_TREK.begin() + 1, req._kcv_TREK.end()); // KCV of TREK changed!
+
+		mInfo._param 	= NULL;
+		mInfo._paramLen = 0;
+		mInfo._type 	= MT_AES_ECB;
+		req._trek_data 	= trek.encrypt(mInfo, data);
+
+		req._sha256Data = pC->generateSHA256(data);
+		req._trak_sha256Data = trak.encrypt(mInfo, req._sha256Data);
+
+		// testee
+		LOG4CXX_INFO(g_loggerTest, "Test prepared");
+		ProcessFirstResponse resp;
+		int err = _pSafenet->processFirst(req, resp); // Should throw exception here
+	}, ExceptionCryptoki);
+}
+
 
 TEST_F(SafenetHelperTests, negative_processFirst_invalid_key_type) {
 	char _data[] = {
